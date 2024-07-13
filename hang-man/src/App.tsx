873e0bc6d9ff55a6cf531c3e9d1ab15dd38eda00
spouki <iyadahmedhamoudi@gmail.com>
2024-07-13 13:33:33 +0100

    creating the start init page

--- hang-man/src/App.tsx
+++ hang-man/src/App.tsx
@@ -5,7 +5,25 @@ function App(){
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random()) * words.length]
   })
-  return <h1>spouki</h1>
+
+  const [gueessedLetters , setGuessedLetters ] = useState<string[]>([])
+
+
+  return <div style={{
+    maxWidth : "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin : "0 auto",
+    alignItems:"center"
+  }}>
+    <div style={{
+      fontSize:"2rem", textAlign: "center"
+    }}>
+       Lose Win
+    </div>
+
+  </div>
 }
 
 export default App
