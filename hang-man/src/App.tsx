cbdc8abebd30e6acea232ea7247eaf7fd148f1ad
spouki <iyadahmedhamoudi@gmail.com>
2024-07-13 12:26:53 +0100

    config files

---
+++ hang-man/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import words from "./wordlist.json"
+
+function App(){
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random()) * words.length]
+  })
+  return <h1>spouki</h1>
+}
+
+export default App
