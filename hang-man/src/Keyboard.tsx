8593354d83a8248df11f03fa58c9b666701fb73e
spouki <iyadahmedhamoudi@gmail.com>
2024-07-13 14:42:27 +0100

    creating the start init page

--- hang-man/src/Keyboard.tsx
+++ hang-man/src/Keyboard.tsx
@@ -1,3 +1,19 @@
+const KEYS = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+  ]
+
 export function Keyboard(){
-    
+     return <div style={{
+        display: "grid",
+        gridTemplateColumns : "repeat (auto-fit , mimax(75px,1fr)) ",
+        gap : ".5rem",
+
+     }}>
+        {KEYS.map(key => {
+            return (
+                <button key={key}>{key}</button>
+            )
+        })}
+     </div>
 }
